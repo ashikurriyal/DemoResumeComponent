b78eeb4a5087dad375bb8c02f036ef0c1dda9885
ashikurriyal <riyal.ashikur@gmail.com>
2024-10-20 23:58:44 +0600

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
         <div>
             <div className="lg:mx-32 mx-6 space-y-20">
                 {/* <JobsSection></JobsSection> */}
+                <h1>New</h1>
             </div>
         </div>
     );
